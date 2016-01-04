5cdf859c5d4f848099c41ca36cd2b7a1778c5928
Mariana Dateș <Mariana Date>
2016-01-04 15:34:16 +0100

    The database

---
+++ farmers_stock.sql
@@ -0,0 +1,132 @@
+-- phpMyAdmin SQL Dump
+-- version 4.2.7
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost:8889
+-- Generation Time: Jan 04, 2016 at 02:33 PM
+-- Server version: 5.5.41-log
+-- PHP Version: 5.6.8
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `farmers_stock`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+CREATE TABLE IF NOT EXISTS `admin` (
+  `admin` varchar(25) NOT NULL,
+  `pass` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ads`
+--
+
+CREATE TABLE IF NOT EXISTS `ads` (
+  `ID_ad` int(11) NOT NULL,
+  `ID_field` int(11) NOT NULL,
+  `ad_text` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `field`
+--
+
+CREATE TABLE IF NOT EXISTS `field` (
+  `ID_field` int(11) NOT NULL,
+  `field_name` varchar(25) NOT NULL,
+  `field_type` varchar(25) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `field`
+--
+
+INSERT INTO `field` (`ID_field`, `field_name`, `field_type`) VALUES
+(1, 'eggs', '1'),
+(2, 'milk', '1'),
+(3, 'cheese', '1'),
+(4, 'vegetables', '2');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `requests`
+--
+
+CREATE TABLE IF NOT EXISTS `requests` (
+  `ID_req` int(100) NOT NULL,
+  `add` varchar(100) NOT NULL,
+  `date` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE IF NOT EXISTS `users` (
+  `ID_user` int(11) NOT NULL,
+  `username` varchar(20) NOT NULL,
+  `password` varchar(30) NOT NULL,
+  `address` varchar(100) NOT NULL,
+  `phone` varchar(15) NOT NULL,
+  `first_name` varchar(25) NOT NULL,
+  `last_name` varchar(25) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+ ADD UNIQUE KEY `admin` (`admin`);
+
+--
+-- Indexes for table `ads`
+--
+ALTER TABLE `ads`
+ ADD PRIMARY KEY (`ID_ad`), ADD UNIQUE KEY `ID_ad` (`ID_ad`);
+
+--
+-- Indexes for table `field`
+--
+ALTER TABLE `field`
+ ADD PRIMARY KEY (`ID_field`);
+
+--
+-- Indexes for table `requests`
+--
+ALTER TABLE `requests`
+ ADD PRIMARY KEY (`ID_req`), ADD UNIQUE KEY `ID_req` (`ID_req`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+ ADD PRIMARY KEY (`ID_user`), ADD UNIQUE KEY `ID_user` (`ID_user`);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
